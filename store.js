e9ca4dcb8ed39473b24d6f8a8c643901d973dcbe
Marko Grujic <marko.grujic@rocketmail.com>
2025-05-04 15:40:06 +0200

    added form function to add new items in table

--- store.js
+++ store.js
@@ -42,4 +42,38 @@ articals.forEach((artical, index) => {
 });
 
 
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const naziv = document.getElementById('naziv').value;
+    const cena = document.getElementById('cena').value;
+    const opis = document.getElementById('opis').value;
+    
+    const noviArtikal = new Artical(naziv, cena, opis);
+    articals.push(noviArtikal);
+    
+    const tr = document.createElement("tr");
+    const br = document.createElement("td");
+    const nazivTd = document.createElement("td");
+    const cenaTd = document.createElement("td");
 
+    br.textContent = articals.length;
+    nazivTd.textContent = noviArtikal.naziv;
+    cenaTd.textContent = noviArtikal.cena;
+
+    tr.appendChild(br);
+    tr.appendChild(nazivTd);
+    tr.appendChild(cenaTd);
+
+    table.appendChild(tr);
+    
+    document.getElementById('naziv').value = '';
+    document.getElementById('cena').value = '';
+    document.getElementById('opis').value = '';
+    
+    tr.addEventListener('click', function() {
+        document.querySelector('.details tr:nth-child(1) div').textContent = `Naziv: ${noviArtikal.naziv}`;
+        document.querySelector('.details tr:nth-child(2) div').textContent = `Cena: ${noviArtikal.cena}$`;
+        document.querySelector('.details tr:nth-child(3) div').textContent = `Opis: ${noviArtikal.opis}`;
+    });
+});
